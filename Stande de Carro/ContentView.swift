def308620409155f64774ca2aedf58950ab28cfd
Mauro de Oliveira Andre <mauroolivere@hotmail.com>
2022-02-18 13:45:04 +0100

    Initial Commit

---
+++ Stande de Carro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stande de Carro
+//
+//  Created by Mauro Sys One on 2/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stande de Carro/Stande_de_CarroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stande_de_CarroApp.swift
+//  Stande de Carro
+//
+//  Created by Mauro Sys One on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Stande_de_CarroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
